a149980440ae4a0007853305e249bfd6ad98e716
Migalv <miguel@mpower.africa>
2020-11-30 12:05:44 +0100

    Add Firebase Firestore package
    Also create Firestore helpers and Firestore Collection Keys class to store
    the firestore collection keys

---
+++ lib/infrastructure/core/firestore_collection_keys.dart
@@ -0,0 +1,3 @@
+abstract class FirestoreCollectionKeys {
+  static const String customers = "customers";
+}
---
+++ lib/infrastructure/core/firestore_helpers.dart
@@ -0,0 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sales_app/infrastructure/core/firestore_collection_keys.dart';
+
+extension FirestoreX on FirebaseFirestore {
+  CollectionReference get customersCollection =>
+      collection(FirestoreCollectionKeys.customers);
+}
