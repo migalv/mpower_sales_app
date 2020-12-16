c1be89568d3df3a1db2bd8fce5effa1afb1949dd
Migalv <miguel@mpower.africa>
2020-12-16 14:36:57 +0100

    Create the Market entity in the Domain layer

--- lib/infrastructure/markets/market_remote_data_source.dart
+++ lib/infrastructure/markets/market_remote_data_source.dart
@@ -3,15 +3,16 @@ import 'package:dartz/dartz.dart';
 import 'package:injectable/injectable.dart';
 import 'package:sales_app/domain/core/data_sources/cached_remote_data_source_failure.dart';
 import 'package:sales_app/domain/core/data_sources/i_cached_remote_data_source.dart';
+import 'package:sales_app/domain/markets/market.dart';
 import 'package:sales_app/domain/teams/team.dart';
 import 'package:sales_app/infrastructure/markets/market_dto.dart';
 import 'package:sales_app/infrastructure/core/firestore_helpers.dart';
 
 @LazySingleton(as: ICachedRemoteDataSource)
-class MarketRemoteDataSource implements ICachedRemoteDataSource<MarketDTO> {
+class MarketRemoteDataSource implements ICachedRemoteDataSource<Market> {
   final FirebaseFirestore _firestore;
   final ICachedRemoteDataSource<List<Team>> _teamRemoteDataSource;
-  MarketDTO _cachedMarket;
+  Market _cachedMarket;
 
   /// Remote data source that uses Firestore to retrieve the Market for the user
   ///
@@ -19,7 +20,7 @@ class MarketRemoteDataSource implements ICachedRemoteDataSource<MarketDTO> {
   MarketRemoteDataSource(this._firestore, this._teamRemoteDataSource);
 
   @override
-  Future<Either<CachedRemoteDataSourceFailure, MarketDTO>> get() async {
+  Future<Either<CachedRemoteDataSourceFailure, Market>> get() async {
     if (_cachedMarket != null) return Right(_cachedMarket);
 
     final result = await _teamRemoteDataSource.get();
@@ -35,7 +36,8 @@ class MarketRemoteDataSource implements ICachedRemoteDataSource<MarketDTO> {
             .get();
 
         if (snapshot.docs.isNotEmpty) {
-          _cachedMarket = MarketDTO.fromFirestore(snapshot.docs.first);
+          _cachedMarket =
+              MarketDTO.fromFirestore(snapshot.docs.first).toDomain();
         } else {
           return const Left(CachedRemoteDataSourceFailure.elementNotFound());
         }
