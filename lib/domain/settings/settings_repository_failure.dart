3388219a09b2ecfacfeb18c81d597c44816bec57
Migalv <miguel@mpower.africa>
2020-12-17 10:05:21 +0100

    Add default messages 4 SettingsRepositoryFailure

--- lib/domain/settings/settings_repository_failure.dart
+++ lib/domain/settings/settings_repository_failure.dart
@@ -3,7 +3,10 @@ import 'package:freezed_annotation/freezed_annotation.dart';
 part 'settings_repository_failure.freezed.dart';
 
 @freezed
-abstract class SettingsRepositoryFailure with _$SettingsRepositoryFailure {
+abstract class SettingsRepositoryFailure
+    implements _$SettingsRepositoryFailure {
+  const SettingsRepositoryFailure._();
+
   /// Failure when an unexpected exception is thrown in the CustomerRepository
   const factory SettingsRepositoryFailure.unexpectedException({
     @required Exception exception,
@@ -19,4 +22,25 @@ abstract class SettingsRepositoryFailure with _$SettingsRepositoryFailure {
     /// The key of the not existing setting
     @required String settingKey,
   }) = SettingNotExists;
+
+  /// Returns a list with the default error messages for each failure
+  List<String> get defaultErrorMessages {
+    return map(
+      unexpectedException: (failure) => [
+        "UnexpectedException Failure occured",
+        "Exception: ${failure.exception}",
+        "StackTrace: ${failure.stackTrace}",
+      ],
+      uninitializedSettings: (failure) => [
+        "UninitializedSettings Failure occured",
+        "Fetch the settings first...",
+        "If this error is shown, please contact with the software team",
+      ],
+      settingNotExists: (failure) => [
+        "SettingNotExists Failure occurred",
+        "Tried to fetch ${failure.settingKey} setting.",
+        "This setting does not exist in the data base",
+      ],
+    );
+  }
 }
